8632c29b413648c0aa4e83998952e36256045ba5
Amaan Ullah <amaankhanse@gmail.com>
2024-11-12 19:25:04 +0500

    add react navigation

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={Home}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function Home() {
   return (
     <View style={styles.container}>
       <Text>Sample React Native Project</Text>
