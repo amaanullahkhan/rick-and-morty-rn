9cca5a474fd7c8a2f469a18d774a819ee2046842
Amaan Ullah <amaankhanse@gmail.com>
2024-11-12 20:32:52 +0500

    add tabs navigator

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { NavigationContainer, useNavigation, useRoute } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home' screenOptions={{ headerStyle: {backgroundColor: 'tomato'}}}>
-        <Stack.Screen name='Info' component={Info}/>
-        <Stack.Screen name='Home' component={Home} options={{title: 'Home - RN' }}/>
-      </Stack.Navigator>
+      <TabsLayout />
     </NavigationContainer>
   );
+}
+
+function StackLayout() {
+  return (
+    <Stack.Navigator initialRouteName='Home' screenOptions={{ headerStyle: {backgroundColor: 'tomato'}}}>
+        <Stack.Screen name='Info' component={Info}/>
+        <Stack.Screen name='Home' component={Home} options={{title: 'Home - RN' }}/>
+    </Stack.Navigator>
+  );
+}
+
+function TabsLayout() {
+  return (
+    <Tab.Navigator>
+    <Tab.Screen name='Home' component={Home}></Tab.Screen>
+    <Tab.Screen name='Info' component={Info}></Tab.Screen>  
+  </Tab.Navigator>
+  )
 }
 
 function Home() {
